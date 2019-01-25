36229e8a0b1f6f528736e52d039d86105673a9df
dbulka <dzm.bulka@gmail.com>
2019-01-25 20:21:44 +0300

    task

---
+++ model/admin.py
@@ -0,0 +1,6 @@
+from user import User
+
+
+class Admin(User):
+
+    def make_chamnge(self, *args, **kwargs)
---
+++ model/user.py
@@ -0,0 +1,7 @@
+class User:
+
+    def __init__(self, *args, **kwargs):
+
+    def fill_text_bottom(self, *args, **kwargs)
+
+    def admin_access(self, *args, **kwargs)
